528c36989071ec0fc9d589762c58680979086955
Ante Bikić <104563758+abikic7@users.noreply.github.com>
2022-05-17 11:18:39 +0200

    Create edunovapp25.sql
    Završetak osnovnog unosa

---
+++ edunovapp25.sql
@@ -0,0 +1,158 @@
+# linija koja počne s hash (shift + 3) je komentar
+# Ovo je SQL skripta
+# naredba za izvođenje
+# C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8  < C:\Users\dell\Documents\EdunovaPP25\SQL\edunovapp25.sql
+# C:\xampp\mysql\bin\mysql -uroot < C:\Bikan\Desktop\materijali s nastave\EdunovaPP25-6e5664ee896ba3cdc64f2eff4ddcf263b244ecdb\EdunovaPP25-6e5664ee896ba3cdc64f2eff4ddcf263b244ecdb\SQL\edunovapp25.sql
+
+drop database if exists edunovapp25;
+create database edunovapp25 default charset utf8mb4;
+use edunovapp25;
+create table smjer(
+    sifra int not null primary key auto_increment,
+    naziv varchar(50) not null,
+    cijena decimal(18,2), # kada ne piše not null podrazumjeva se null
+    trajanje int,
+    upisnina decimal(18,2),
+    certificiran boolean
+);
+
+create table grupa(
+    sifra int not null primary key auto_increment,
+    naziv varchar(20) not null,
+    datumpocetka datetime,
+    maksimalnopolaznika int,
+    smjer int not null, 
+    predavac int 
+);
+
+create table clan(
+    sifra int not null primary key auto_increment,
+    grupa int not null, 
+    polaznik int not null 
+);
+
+create table polaznik(
+    sifra int not null primary key auto_increment,
+    osoba int not null, 
+    brojugovora varchar(10)
+);
+
+create table predavac(
+    sifra int not null primary key auto_increment,
+    osoba int not null, 
+    iban varchar(50)
+);
+
+create table osoba(
+    sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    email varchar(100),
+    oib char(11)
+);
+
+# definiranje vanjskih ključeva
+alter table grupa add foreign key (smjer) references smjer(sifra);
+alter table grupa add foreign key (predavac) references predavac(sifra);
+
+alter table clan add foreign key (grupa) references grupa(sifra);
+alter table clan add foreign key (polaznik) references polaznik(sifra);
+
+alter table polaznik add foreign key (osoba) references osoba(sifra);
+alter table predavac add foreign key (osoba) references osoba(sifra);
+
+
+
+# unos podataka
+insert into smjer (naziv)
+values ('PHP programiranje Č');
+# loš način
+# 1 - dobivena vrijednost primarnog ključa
+insert into smjer values 
+(null,'PHP programiranje',5999.99,130,null,false);
+
+insert into grupa (naziv, smjer)
+values ('PP25',1);
+# bolji način
+# 2
+insert into smjer(naziv,cijena) 
+values ('Java programiranje',5999.99);
+
+# primjer dobre prakse
+# 3
+insert into 
+smjer(sifra,naziv,cijena,trajanje,
+upisnina,certificiran)
+values (null,'Računovodstvo',4000,120,
+300,true);
+
+
+# 1
+insert into grupa 
+(sifra,naziv,datumpocetka,maksimalnopolaznika,
+smjer,predavac)
+values 
+(null,'PP25','2022-04-23',20,1,null);
+
+insert into grupa
+(sifra,naziv,datumpocetka,maksimalnopolaznika,
+smjer,predavac)
+values
+(null,'JP26','2022-04-23',20,2,null);
+
+
+# 1
+insert into osoba (sifra,ime,prezime,email,oib)
+values (null,'Tomislav','Jakopec','tjakopec@gmail.com',null);
+
+# unijeti osobu Shaquille O'Neal
+# 2
+insert into osoba (sifra,ime,prezime,email,oib)
+values (null,'Shaquille','O''Neal','saki@gmail.com',null);
+
+# jedna insert naredba može unositi više redaka u tablicu
+# 3 - 17
+insert into osoba (sifra,ime,prezime,email,oib)
+values
+(null,'Todorović','Filip','filip.tod95@gmail.com',null),
+(null,'Majer','Antonio','majer.antonio@gmail.com',null),
+(null,'Filipović','Srđan','srdjanfilipovic991@gmail.com',null),
+(null,'Krnjaković','Filip','f.krnja@gmail.com',null),
+(null,'Jukić','Željko','zeljac00@icloud',null),
+(null,'Pavlinušić','Ante','ante.pavlinusic@gmail.com',null),
+(null,'Bikić','Ante','bikic.tm@gmail.com',null),
+(null,'Rukavina','Antun','antunrukavina@hotmail',null),
+(null,'Poljak','Dino','dinopoljak99@gmail.com',null),
+(null,'Rous','Leonard','leonardrous123@gmail.com',null),
+(null,'Šakić','Marija','sakicmarija35@gmail.com',null),
+(null,'Bukovec','Boris','botaosijek@gmail.com',null),
+(null,'Kovač','Filip','chilim.dj@gmail.com',null),
+(null,'Vuletić','Antonio','avuletic2212@gmail.com',null),
+(null,'Andrija','Kruhoberec','akruhoberec1@outlook.com',null);
+
+# 1 - 15
+
+insert into polaznik (sifra,osoba,brojugovora)
+values
+(null,3,null),
+(null,4,null),
+(null,5,null),
+(null,6,null),
+(null,7,null),
+(null,8,null),
+(null,9,null),
+(null,10,null),
+(null,11,null),
+(null,12,null),
+(null,13,null),
+(null,14,null),
+(null,15,null),
+(null,16,null),
+(null,17,null);
+
+
+
+insert into clan (grupa,polaznik)
+values (1,1),(1,2),(1,3),(1,4),(1,5),
+(1,6),(1,7),(1,8),(1,9),(1,10),
+(1,11),(1,12),(1,13),(1,14),(1,15);
