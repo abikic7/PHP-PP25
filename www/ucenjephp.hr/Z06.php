307d4954ddffcfa97b1a73c1a031b445daf7ecf9
Ante Bikić <104563758+abikic7@users.noreply.github.com>
2022-06-29 10:37:48 +0200

    Create Z06.php

---
+++ www/ucenjephp.hr/Z06.php
@@ -0,0 +1,27 @@
+<?php
+
+
+
+
+
+// Stranica prima cijeli broj.
+// ako je broj paran boja stranice treba biti zelena
+// inač treba biti crvena
+
+$a = isset($_GET['a']) ? $_GET['a'] : 0;
+
+if (($a % 1)===0) {
+    
+    
+    echo '<body style="background-color: green">';
+
+} else {
+     
+    
+    echo '<body style="background-color: red">';
+
+}
+
+
+
+?>
