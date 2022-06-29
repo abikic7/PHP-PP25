4b258dd5e086698a82f39fcec9bacc5c86d8af51
Ante Bikić <104563758+abikic7@users.noreply.github.com>
2022-06-29 11:44:35 +0200

    Z07

--- www/ucenjephp.hr/Z07.php
+++ www/ucenjephp.hr/Z07.php
@@ -14,11 +14,43 @@
 // ulaz: -1 0 -1
 // izlaz: 0
 
-$p1 = isset($_GET['p1']) ? $_GET['p1'] : 92; 
-$p2 = isset($_GET['p2']) ? $_GET['p2'] : 3; 
-$p3 = isset($_GET['p3']) ? $_GET['p3'] : 0;
 
-if($p1>$p2)
-echo $p1;
+$a = isset($_GET['a']) ? $_GET['a'] : 3;
+$b = isset($_GET['b']) ? $_GET['b'] : 8;
+$c = isset($_GET['c']) ? $_GET['c'] : 92;
+
+
+if($a > $b && $a > $c){
+    echo 'Najveći broj je ' . $a;
+} else if ($b > $a && $b > $c) {
+    echo 'Najveći broj je ' . $b;
+} else {
+    echo 'Najveći broj je ' . $c, '<hr />';
+}
+
+$a = isset($_GET['a']) ? $_GET['a'] : 3;
+$b = isset($_GET['b']) ? $_GET['b'] : 3;
+$c = isset($_GET['c']) ? $_GET['c'] : 2;
+
+
+if($a > $b && $a > $c){
+    echo 'Najveći broj je ' . $a;
+} else if($b > $a && $b > $c){
+    echo 'Najveći broj je ' . $b;
+} else {
+    echo 'Najveći broj je ' . $c, '<hr />';
+}
+
+$x = isset($_GET['x']) ? $_GET['x'] : -1;
+$y = isset($_GET['y']) ? $_GET['y'] : 0;
+$z = isset($_GET['z']) ? $_GET['z'] : -1;
+
+if($x > $y && $x > $z){
+    echo 'Najveći broj je ' . $x;
+} else if($y > $x && $y > $z){
+    echo 'Najveći broj je ' . $y;
+} else {
+    echo 'Najveći broj je ' . $z;
+}
 
 
